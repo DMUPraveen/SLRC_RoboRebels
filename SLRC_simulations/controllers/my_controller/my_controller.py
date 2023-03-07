28d63bf5513d466741e173a2185f628991d53811
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-03-07 10:28:01 +0530

    AdjustedTheSize

--- SLRC_simulations/controllers/my_controller/my_controller.py
+++ SLRC_simulations/controllers/my_controller/my_controller.py
@@ -68,29 +68,21 @@ def armInit(robot):
 
 # get the time step of the current world.
 def run_robot(robot):
-    max_velocity=6.28
+    max_velocity=-6.28
    
     timestep = int(robot.getBasicTimeStep())
     
     left_motor1 = robot.getDevice("Left_motor1")
-    left_motor2 = robot.getDevice("Left_motor2")
     
     right_motor1 = robot.getDevice("Right_motor1")
-    right_motor2 = robot.getDevice("Right_motor2")
     
     left_motor1.setPosition(float("inf"))
-    left_motor2.setPosition(float("inf"))
     
     right_motor1.setPosition(float("inf"))
-    right_motor2.setPosition(float("inf"))
-    
-    
+
     while robot.step(timestep) != -1:
         left_motor1.setVelocity(max_velocity)
-        left_motor2.setVelocity(max_velocity)
-        
         right_motor1.setVelocity(max_velocity)
-        right_motor2.setVelocity(max_velocity)
         
         pass
 
