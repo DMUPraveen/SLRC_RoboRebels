25764805e2f71697dd996a8bfa1f28fe72a8026f
DMUPraveen <dmu.praveen@gmail.com>
2023-03-14 12:37:14 +0530

    Basic Line follow implmented

--- Line_Follow/Line_Follow.ino
+++ Line_Follow/Line_Follow.ino
@@ -1,4 +1,6 @@
 #include "LineSensor.h"
+#include "PID.h"
+#include "Motor_Controller.h"
 
 const uint8_t line_sensor_size = 8;
 const uint8_t lineSensorPins[8] = {
@@ -12,23 +14,52 @@ const uint8_t lineSensorPins[8] = {
     GPIO_NUM_36,
 
 };
+const uint8_t motor_in1 = 32;
+const uint8_t motor_in2 = 33;
+const uint8_t motor_in3 = 25;
+const uint8_t motor_in4 = 26;
+const uint32_t calibration_iterations = 10000;
+LineSensor ls = LineSensor(lineSensorPins);
 
-LineSensor ls = LineSensor(lineSensorPins, line_sensor_size);
+Motor_Controller mt = Motor_Controller(
+    motor_in1,
+    motor_in2,
+    motor_in3,
+    motor_in4);
+
+PID pid = PID(1.0f, 0.0f, 0.0f, 0.0f);
 void setup()
 {
     Serial.begin(9600);
+    pinMode(BUILTIN_LED, OUTPUT);
+    delay(5000);
+    Serial.println("Begin calibrating");
+    digitalWrite(BUILTIN_LED, HIGH);
+    ls.calibrate(calibration_iterations);
+    digitalWrite(BUILTIN_LED, LOW);
+    Serial.println("End calibrating");
+    delay(5000);
+    pid.set_point(0.0);
 }
 
 void loop()
 {
     ls.update();
-    Serial.printf("%f,%f,%f,%f,%f,%f,%f,%f\n",
-                  ls.m_readings[0],
-                  ls.m_readings[1],
-                  ls.m_readings[2],
-                  ls.m_readings[3],
-                  ls.m_readings[4],
-                  ls.m_readings[5],
-                  ls.m_readings[6],
-                  ls.m_readings[7]);
+    // Serial.printf(
+    //     "%i %i %i %i %i %i %i %i %f\n",
+    //     ls.m_linedetected[0],
+    //     ls.m_linedetected[1],
+    //     ls.m_linedetected[2],
+    //     ls.m_linedetected[3],
+    //     ls.m_linedetected[4],
+    //     ls.m_linedetected[5],
+    //     ls.m_linedetected[6],
+    //     ls.m_linedetected[7],
+    //     ls.m_line_detection_threshold);
+    float measurement = ls.calculate_line_position();
+    float control_signal = pid.control(measurement, 1.0f);
+    mt.set_motor_speed(control_signal, control_signal);
+    Serial.printf(
+        "measurement : %f,control_signal: %f\n",
+        measurement, control_signal);
 }
--- Line_Follow/PID.h
+++ Line_Follow/PID.h
@@ -1,52 +1,103 @@
 #ifndef PID_HEADER
 #define PID_HEADER
 
-struct PID_data{
+void unit_float_clamp(float &value)
+{
+    // clamps a value between -1 and 1 (by reference)
+    if (value > 1.0f)
+    {
+        value = 1.0f;
+    }
+
+    if (value < -1.0f)
+    {
+        value = -1.0f;
+    }
+}
+
+struct PID_data
+{
     float K_P;
     float K_I;
     float K_D;
+    float Derivative_filter;
 };
+
 struct PID
 {
 
     float m_set_point = 0.0f;
+
+    // gain values
     float m_K_P = 0.0f;
     float m_K_D = 0.0f;
     float m_K_I = 0.0f;
-    float I_error = 0.0f;
 
-    PID(){}
-    PID(PID_data data){
+    float m_derivative_filter_constant = 0.0f; // when 0 there is no filtering /1 meanse noe derivative calculations
+    // must be between 0 and 1
+
+    // vairables used for calculation puposes
+    float m_I_error = 0.0f;   // integration of the error
+    float m_pre_error = 0.0f; // previous error
+    float m_D_error = 0.0f;   // derivative term
+
+    PID()
+    {
+    }
+    PID(PID_data data) // used for setting data from a external packet
+    {
         m_K_P = data.K_P;
         m_K_I = data.K_I;
         m_K_D = data.K_D;
+        m_derivative_filter_constant = data.Derivative_filter;
     }
 
-    PID(float K_P, float K_D, float K_I)
-        : m_K_P(K_P), m_K_D(K_D), m_K_I(K_I)
+    PID(float K_P, float K_D, float K_I, float derivative_filter)
+        : m_K_P(K_P), m_K_D(K_D), m_K_I(K_I), m_derivative_filter_constant(derivative_filter)
     {
     }
 
-    void set_point(float set_point){
+    void reset()
+    {
+
+        m_I_error = 0.0f;
+        m_pre_error = 0.0f;
+        m_D_error = 0.0f;
+    }
+
+    void set_point(float set_point)
+    {
         m_set_point = set_point;
     }
 
-    float control(float measurement,float delta){
+    float control(float measurement, float delta)
+    {
+        /*
+            Returns a value between 1 and -1
+        */
 
         float error = m_set_point - measurement;
 
+        m_I_error = m_I_error + delta * error;
 
-        if ( (I_error > 1 && error > 0) || ( I_error < -1 && error < 0 ))
-        {  }
+        //---------------integrator anti-windup-------------------
+        unit_float_clamp(m_I_error); // not ideal but will work for now
+        //--------------------------------------------------------
 
-        else
-        { I_error = I_error + delta*error;}
-        
-        float signal = m_K_P * error + m_K_I * I_error;
+        float new_derivative = (error - m_pre_error) / delta;
+
+        m_D_error = m_derivative_filter_constant * m_D_error + (1 - m_derivative_filter_constant) * new_derivative;
+
+        // caluclating the signal
+        float signal = m_K_P * error + m_K_I * m_I_error + m_K_D * m_D_error;
+
+        unit_float_clamp(signal);
+
+        // storing values for next calculation
+        m_pre_error = error;
+
         return signal;
-        
     }
 };
 
-    
 #endif
