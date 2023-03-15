a9753d5ee892457b57e47d9ec4cd7b27a1006c30
Aaftab Naim <aaftabnaim@gmail.com>
2023-03-15 20:06:32 +0530

    Cleaned Directory and World

--- arrow_follower/controllers/ePuck_controller/ePuck_controller.py
+++ arrow_follower/controllers/ePuck_controller/ePuck_controller.py
@@ -5,16 +5,16 @@ sys.path.insert(0,'E:\\DEEE\\4th SEM\\SLRC 23\\root\\deploy_scripts')
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-#from controller import Robot
+from controller import Robot
 import feature_extractor
 import preprocessor
 # create the Robot instance.
-#robot = Robot()
+robot = Robot()
 
 # get the time step of the current world.
-#timestep = int(robot.getBasicTimeStep())
+timestep = int(robot.getBasicTimeStep())
 
-"""
+
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     # Read the sensors:
@@ -28,4 +28,3 @@ while robot.step(timestep) != -1:
     pass
 
 # Enter here exit cleanup code.
-"""
