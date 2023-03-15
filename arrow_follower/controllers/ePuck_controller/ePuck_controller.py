b3a707476a40bd8487ba53f98536a238ccba3290
Aaftab Naim <aaftabnaim@gmail.com>
2023-03-15 19:47:32 +0530

    Webots Simulation

---
+++ arrow_follower/controllers/ePuck_controller/ePuck_controller.py
@@ -0,0 +1,31 @@
+"""ePuck_controller controller."""
+
+import sys
+sys.path.insert(0,'E:\\DEEE\\4th SEM\\SLRC 23\\root\\deploy_scripts')
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+#from controller import Robot
+import feature_extractor
+import preprocessor
+# create the Robot instance.
+#robot = Robot()
+
+# get the time step of the current world.
+#timestep = int(robot.getBasicTimeStep())
+
+"""
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
+"""
