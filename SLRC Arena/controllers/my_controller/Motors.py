09a3a3d5415d88ce8c86fd34117d68229d712dfe
DMUPraveen <dmu.praveen@gmail.com>
2023-03-21 23:52:08 +0530

    Moved classes around

--- SLRC Arena/controllers/my_controller/Motors.py
+++ SLRC Arena/controllers/my_controller/Motors.py
@@ -1,12 +1,6 @@
 from typing import Tuple
 from controller import Robot
-from PID import PID
-import math
-
-ROBOT_SCALE = 1.1
-
-WHEEL_RADIUS = 0.03*ROBOT_SCALE
-INTERWHEEL_DISTANE = 0.0720026*ROBOT_SCALE
+import numpy as np
 
 
 class Motorcontrol:
@@ -62,89 +56,11 @@ class Motorcontrol:
         returns the position sensor values of the motors they are decreasing for forward direction
         therefore it is negated the intial value is added so that the starting value will be zero
         '''
-        return (-self.left_position.getValue()+self.start_left, -self.right_position.getValue()+self.start_right)
+        return np.array((-self.left_position.getValue()+self.start_left, -self.right_position.getValue()+self.start_right))
 
+    def get_pos_sensors(self):
+        return self.left_position, self.right_position
 
-def rotational_distance_to_linear(rot_distance_rad: float):
-    '''
-    v = r*omega 
-    '''
-    return rot_distance_rad*WHEEL_RADIUS
-
-
-def rotational_to_robot_rotational(rot_turn_distance: float):
-    return rot_turn_distance*WHEEL_RADIUS/INTERWHEEL_DISTANE
-
-
-def get_rotational_distance_delta(intial_values: Tuple[float, float], final_values: Tuple[float, float]):
-    rotation_distance = [
-        final_values[0] - intial_values[0],
-        final_values[1] - intial_values[1]
-    ]
-    return (rotation_distance[0]+rotation_distance[1])/2
-
-
-def get_rotational_rotation_delta(intial_values: Tuple[float, float], final_values: Tuple[float, float]):
-    rotation_distance = [
-        final_values[0] - intial_values[0],
-        final_values[1] - intial_values[1]
-    ]
-    return (rotation_distance[0]-rotation_distance[1])/2
-
-
-class LinearTraveller:
-    def __init__(self, pid_controller: PID, motorcontroller: Motorcontrol):
-        self.motorcontroller = motorcontroller
-        self.initial_position = (0.0, 0.0)
-        self.target_distance = 0.0
-        self.pid_controller = pid_controller
-
-    def initialize(self, target_distance: float):
-        '''
-        upon intialization it will use the current position and travel a linear distance of target_distance
-        '''
-        self.initial_position = self.motorcontroller.get_position_values()
-        self.target_distance = target_distance
-        self.pid_controller.set_set_point(0)
-
-    def run(self):
-        '''
-        motocontroller.set_pose_speed() must be called after this call to take effect
-        '''
-        current_position = self.motorcontroller.get_position_values()
-        rotational_distance = get_rotational_distance_delta(
-            self.initial_position, current_position)
-        distance = rotational_distance_to_linear(rotational_distance)
-        print(distance)
-        error = self.target_distance - distance
-        signal = self.pid_controller(error)
-        self.motorcontroller.linear_speed = signal
-
-
-class Rotator:
-    def __init__(self, pid_controller: PID, motorcontroller: Motorcontrol):
-        self.motorcontroller = motorcontroller
-        self.initial_position = (0.0, 0.0)
-        self.target_angle = 0.0
-        self.pid_controller = pid_controller
-
-    def initialize(self, target_angle: float):
-        '''
-        upon intialization it will use the current position and travel a linear distance of target_angle
-        '''
-        self.initial_position = self.motorcontroller.get_position_values()
-        self.target_angle = target_angle
-        self.pid_controller.set_set_point(0)
-
-    def run(self):
-        '''
-        motocontroller.set_pose_speed() must be called after this call to take effect
-        '''
-        current_position = self.motorcontroller.get_position_values()
-        rotational_distance = get_rotational_rotation_delta(
-            self.initial_position, current_position)
-        angle = rotational_to_robot_rotational(rotational_distance)
-        print(angle)
-        error = self.target_angle - angle
-        signal = self.pid_controller(error)
-        self.motorcontroller.rotational_speed = signal
+    def pose_stop(self):
+        self.linear_speed = 0
+        self.rotational_speed = 0
