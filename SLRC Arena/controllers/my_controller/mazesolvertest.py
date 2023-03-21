f8777e4c2487695fce1d2cbbfec8b9aa9152688b
DMUPraveen <dmu.praveen@gmail.com>
2023-03-21 23:53:50 +0530

    Tested Back sensor

--- SLRC Arena/controllers/my_controller/mazesolvertest.py
+++ SLRC Arena/controllers/my_controller/mazesolvertest.py
@@ -1,8 +1,8 @@
 from controller import Robot
 from DistanceSensors import DistanceSensors
-from Motors import Motorcontrol, LinearTraveller, Rotator
+from Motors import Motorcontrol
 from PID import PID
-from Navigation import Navigation
+from Navigation import Alingment, LinearTraveller, Rotator
 import math
 
 
@@ -11,14 +11,15 @@ def main():
     timestep = int(robot.getBasicTimeStep())
     distancesensors = DistanceSensors(robot, timestep)
     motorcontrol = Motorcontrol(robot)
-    navigation = Navigation(motorcontrol, distancesensors)
+    alignment = Alingment(motorcontrol, distancesensors)
     linear_traveller_pid = PID(50, 0, 0)
     lineartravelleter = LinearTraveller(linear_traveller_pid, motorcontrol)
     lineartravelleter.initialize(0.36)
     rotator_pid = PID(10, 0, 0)
     rotator = Rotator(rotator_pid, motorcontrol)
     rotator.initialize(-math.pi)
+
     while robot.step(timestep) != -1:
+        # lineartravelleter.run()
+        print(alignment.align_to_back_wall())
         motorcontrol.set_pose_speed()
-        rotator.run()
-        pass
