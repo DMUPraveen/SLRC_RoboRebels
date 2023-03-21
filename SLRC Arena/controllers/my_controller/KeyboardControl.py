e129898791152b8c0973a319b9298dd2546ff710
DMUPraveen <dmu.praveen@gmail.com>
2023-03-21 23:52:44 +0530

    Added keyboard control

--- SLRC Arena/controllers/my_controller/KeyboardControl.py
+++ SLRC Arena/controllers/my_controller/KeyboardControl.py
@@ -1,29 +1,49 @@
-# from controller import Keyboard
-# from ArmController import armcontroll
-
-# arm=armcontroll
-# VELOCITY = 0.1
-# keyboard_control_on = True
-# CONTROL_KEY = ord('Q')
-# actions = {
-#     ord('A'): lambda arm: arm.waistcontrol(arm, VELOCITY),
-#     ord('D'): lambda arm: arm.shouldercontrol(arm, VELOCITY),
-#     ord('S'): lambda arm: arm.elbowcontrol(arm,VELOCITY),
-#     ord('W'): lambda arm: arm.wristcontrol(arm,VELOCITY),
-#     ord('B'): lambda arm: arm.pitchcontrol(arm,VELOCITY)
-# }
+from controller import Keyboard
+from typing import Callable, Dict, List
+from collections import defaultdict
+from Motors import Motorcontrol
 
 
-# def control_arm_via_keyboard(keyboard: Keyboard, arm: arm):
-#     key = None
-#     global keyboard_control_on
-#     key = keyboard.getKey()
-#     if(key == CONTROL_KEY):
-#         keyboard_control_on = not(keyboard_control_on)
-#     if(not keyboard_control_on):
-#         return
-#     if(key in actions):
-#         actions[key](arm)
-#         return
+class KeyboardControl:
+    def __init__(self, keyboard: Keyboard, sampling_time: int):
+        self.actions: Dict[int, List[Callable[[], None]]
+                           ] = defaultdict(lambda: [])
 
-#     arm.stoparm()
+        self.keyboard = keyboard
+        keyboard.enable(sampling_time)
+
+    def register_action(self, key: str, action: Callable[[], None]):
+        self.actions[ord(key)].append(action)
+
+    def execute(self):
+        while True:
+            key = self.keyboard.getKey()
+            if(key == -1):
+                break
+            for action in self.actions[key]:
+                action()
+
+
+def get_control_actions(motor_control: Motorcontrol):
+    def turn_left():
+        motor_control.rotational_speed = 1
+
+    def turn_right():
+        motor_control.rotational_speed = -1
+
+    def go_forward():
+        motor_control.linear_speed = -10
+
+    def go_back():
+        motor_control.linear_speed = 10
+
+    return turn_left, turn_right, go_forward, go_back
+
+
+def create_normal_drive(keyboardControl: KeyboardControl, motorcontrol: Motorcontrol):
+    turn_left, turn_right, go_forward, go_back = get_control_actions(
+        motorcontrol)
+    keyboardControl.register_action('A', turn_left)
+    keyboardControl.register_action('D', turn_right)
+    keyboardControl.register_action('W', go_forward)
+    keyboardControl.register_action('S', go_back)
