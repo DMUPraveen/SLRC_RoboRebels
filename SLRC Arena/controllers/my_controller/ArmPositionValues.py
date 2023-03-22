b1f23f9cbdb33b259b8e09ee2a6b60eec825e242
athulyar99 <99779659+athulyar99@users.noreply.github.com>
2023-03-22 13:07:14 +0530

    @Buddhi19
    Pikup values updated to blue bo

--- SLRC Arena/controllers/my_controller/ArmPositionValues.py
+++ SLRC Arena/controllers/my_controller/ArmPositionValues.py
@@ -1,10 +1,15 @@
 # Positions of the Arm Components
 
 wrist_val = 0
-shoulder_val=0.5
-elbow_val=2.2
+shoulder_val=0.58
+elbow_val=1.9
 WAIST_VAL=-11.0
-pitch_val=0.15
+pitch_val=0.665
 
+# pikup = {
+#     "shoulder" : 0.5,
+#     "elbow" : 1.95,
+#     "pit" : 0.7
+# }
 # Robot Translation
 # 0.49723 -0.9173 0.0301448
